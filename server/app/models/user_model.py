e3ea9199a32cd6b666774a071743eed654cc6de5
PeterC-B <97701071+PeterC-B@users.noreply.github.com>
2026-01-23 09:32:34 +0000

    Setting up first DB table user_model and docker-compose

--- server/app/models/user_model.py
+++ server/app/models/user_model.py
@@ -1,14 +1,18 @@
 # ORM model representation of a user in the database
 import uuid
+from datetime import datetime
 
 from app.extensions import db
-from sqlalchemy import UUID, String
+from sqlalchemy import UUID, DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 
-class UserModel(db.Model):
-    __tablename__ = 'user'
+class UserAccount(db.Model):
+    __tablename__ = 'user_account'
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
