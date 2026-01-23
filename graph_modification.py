9986c0aca0ee3dccbef56103edeccdf4f37dbb4f
NotEthanFranklin74 <chamax123@outlook.com>
2026-01-23 13:52:07 +0000

    Better file and function management ready for integration with Docker and Flask files

--- graph_modification.py
+++ graph_modification.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 import networkx as nx
 import pandas as pd
 
-def addDistanceToDrinkingPlace(edges_gdf : gpd.GeoDataFrame, coords : tuple) -> gpd.GeoDataFrame:
+def add_distance_to_crime_spots(edges_gdf : gpd.GeoDataFrame, coords : tuple) -> gpd.GeoDataFrame:
     '''
     A function that returns a heatmap of where roads are close to places that allow people to drink
     
@@ -42,7 +42,7 @@ def addDistanceToDrinkingPlace(edges_gdf : gpd.GeoDataFrame, coords : tuple) -> 
 
     return edges_gdf
 
-def addFeatureToGraph(graph : nx.MultiDiGraph, line, coords : tuple, feature : list, limit_amenities : bool = True) -> gpd.GeoDataFrame:
+def add_feature_to_graph(graph : nx.MultiDiGraph, line, coords : tuple, feature : list, limit_amenities : bool = True) -> gpd.GeoDataFrame:
     '''
     Adds all features given to a graph of an area
     
@@ -88,39 +88,6 @@ def addFeatureToGraph(graph : nx.MultiDiGraph, line, coords : tuple, feature : l
         alpha=feature[3],
         markersize=feature[4],
     )
-
-def csv_to_dict(filepath : str) -> dict:
-    '''
-    Converts the edges csv file into dictionary with all connections
-    
-    :param filepath: The filepath of the csv file that contains all info on every edge in the graph
-    :type filepath: str
-    :return: A dictionary that contains the 'from_node' as it's keys, and each node this connects to, with the length
-    :rtype: dict
-    '''
-    allEdges = {}
-    csv = pd.read_csv(filepath)
-    for index, row in csv.iterrows():
-        if int(row["from_node"]) not in allEdges:
-            allEdges[int(row["from_node"])] = {int(row["to_node"]) : float(row["length"])}
-        else:
-            allEdges[int(row["from_node"])][int(row["to_node"])] = float(row["length"])
-    return allEdges
-
-def node_csv_to_dict(filepath:str) -> dict:
-    '''
-    Converts a csv file containing every node's information into a dictionary to be read
-    
-    :param filepath: The filepath of the csv file that contains all info on every node in the graph 
-    :type filepath: str
-    :return: A dictionary that contains the 'node_id' as it's keys, and each nodes x and y coordinates as it's tuple value
-    :rtype: dict
-    '''
-    allNodes = {}
-    csv = pd.read_csv(filepath)
-    for index, row in csv.iterrows():
-        allNodes[int(row["node_id"])] = (float(row["x"]), float(row["y"]))
-    return allNodes
 
 def find_edge_path(nodePath: list, edgeTablePath : str) -> list:
     '''
