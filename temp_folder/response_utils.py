79626ba5979a252bf8070f7fb2c5a11b1636e580
PeterC-B <97701071+PeterC-B@users.noreply.github.com>
2026-01-24 14:37:22 +0000

    Integrated most of the code together except for the files inside the temp_folder. These will be integrated soon

--- temp_folder/response_utils.py
+++ temp_folder/response_utils.py
@@ -5,7 +5,6 @@ Response utilities:
     - Route comparison summaries
 """
 
-from flask import jsonify
 
 # Format a single route response for the API
 def format_route_response(path, graph, geometry=None, metadata=None):
@@ -38,12 +37,6 @@ def format_route_response(path, graph, geometry=None, metadata=None):
         "metadata": metadata or {}
     }
 
-
-# Return a standardised error response
-def format_error(message, status=400):
-    response = jsonify({"error": message})
-    response.status_code = status
-    return response
 
 def compare_routes(routes):
     """
