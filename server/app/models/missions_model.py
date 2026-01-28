612b0756c88be37fa0974774cca28d9c8ab597fd
NotEthanFranklin74 <chamax123@outlook.com>
2026-01-28 22:52:09 +0000

    Creating more SQL models for the database

---
+++ server/app/models/missions_model.py
@@ -0,0 +1,18 @@
+# ORM model representation of a user in the database
+import uuid
+
+from sqlalchemy import UUID, String
+from sqlalchemy import Enum as SQLEnum
+from enums.MISSION_TIER import MissionTier
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.extensions import db
+
+class MissionsModel(db.Model):
+    __tablename__ = "missions"
+
+    mission_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    mission_name: Mapped[str] = mapped_column(String(), nullable=False)
+    question: Mapped[str] = mapped_column(String(), nullable=False)
+    answer: Mapped[str] = mapped_column(String(), nullable=False)
+    tier: Mapped[MissionTier] = mapped_column(SQLEnum(MissionTier), nullable=False)
--- server/app/models/nodes_model.py
+++ server/app/models/nodes_model.py
@@ -1,8 +1,5 @@
 # ORM model representation of a user in the database
-import uuid
-from datetime import datetime
-
-from sqlalchemy import UUID, DateTime, String, Float
+from sqlalchemy import Float, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.extensions import db
@@ -10,5 +7,6 @@ from app.extensions import db
 class NodesModel(db.Model):
     __tablename__= 'nodes'
 
-    node_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True))
-    x_coordinate: Mapped[float] = mapped_column(Float())
+    node_id: Mapped[int] = mapped_column(Integer(), nullable=False, primary_key=True)
+    x_coordinate: Mapped[float] = mapped_column(Float(15), nullable=False)
+    y_coordinate: Mapped[float] = mapped_column(Float(15), nullable=False)
