72b57f235e17dc2065960e8a9952e0f30b04eef7
NotEthanFranklin74 <chamax123@outlook.com>
2026-01-29 17:40:20 +0000

    Fixing surface quality weights and lighting weights

--- server/scripts/visualisation/test_plot.py
+++ server/scripts/visualisation/test_plot.py
@@ -1,4 +1,4 @@
 from server.scripts.visualisation.visualisation_utils import plot_blank_graph
 
-coords = (51.4545, -2.5879)  # Bristol
+coords = (51.460498, -2.585757)  # Bristol
 plot_blank_graph(coords, radius=500, travel_type="walk", saveToFile=True)
