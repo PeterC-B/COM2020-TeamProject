f8c097328e22c998ad79debe3063f6eefc1b7a55
NotEthanFranklin74 <chamax123@outlook.com>
2026-02-01 20:26:45 +0000

    Created weight calculation based on preferences

--- server/app/extensions/__init__.py
+++ server/app/extensions/__init__.py
@@ -1,10 +1,9 @@
-from server.app.models.Base import Base
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 
 migrate = Migrate()
 
