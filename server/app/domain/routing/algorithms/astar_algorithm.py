20ac2d8e634783628f9663abb9a246ac493a6ba5
NotEthanFranklin74 <chamax123@outlook.com>
2026-02-02 15:27:19 +0000

    Added file tree and started to edit algorithms

--- server/app/domain/routing/algorithms/astar_algorithm.py
+++ server/app/domain/routing/algorithms/astar_algorithm.py
@@ -1,4 +1,4 @@
-import heapq
+'''import heapq
 from math import inf
 import networkx as nx
 
@@ -73,4 +73,4 @@ def astar(graph: nx.MultiDiGraph, source, target, cost_function, heuristic_funct
 
                 heapq.heappush(pq, (new_f_score, neighbour))
 
-    return inf, []
+    return inf, []'''
