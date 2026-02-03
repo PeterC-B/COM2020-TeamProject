4a6c4353ca36f942a5fe345e0350d8432ee90534
NotEthanFranklin74 <chamax123@outlook.com>
2026-02-03 16:53:54 +0000

    Created route algorithm for connecting back-end and front-end

--- server/app/api/helpers/algorithm_helper.py
+++ server/app/api/helpers/algorithm_helper.py
@@ -2,23 +2,96 @@ from server.app.domain.routing.algorithms.yen_algorithm import yens
 from server.app.domain.routing.algorithms.dijkstra_algorithm import dijkstra
 import networkx as nx
 import osmnx as ox
+import pandas as pd
+from shapely import wkt
+from shapely.geometry import Point
+from shapely.ops import unary_union
+import geopandas as gpd
 
 def get_dict_of_edges(graph : nx.MultiDiGraph):
     _, edges_gdf = ox.graph_to_gdfs(graph)
     edges_gdf = edges_gdf.reset_index()
-    edges_gdf = edges_gdf[[
-        "u", "v", "key",
-        "length",
-    ]]
-    edges = edges_gdf.to_dict(orient="records")
+
     all_edges = {}
-    for edge in edges:
-        u = edge["u"]
-        v = edge["v"]
-        length = edge["length"]
 
-        if u not in all_edges:
-            all_edges[u] = {}
+    for _, row in edges_gdf.iterrows():
+        u = row["u"]
+        v = row["v"]
+        length = row["length"]
 
-        all_edges[u][v] = length
+        all_edges.setdefault(u, {})
+
+        if v not in all_edges[u] or length < all_edges[u][v]:
+            all_edges[u][v] = length
+
     return all_edges
+
+def nodes_csv_to_gdf(csv_path: str = "client/public/nodes_table.csv", crs="EPSG:4326") -> gpd.GeoDataFrame:
+    df = pd.read_csv(csv_path)
+
+    geometry = [
+        Point(xy) for xy in zip(df["x"], df["y"])
+    ]
+
+    gdf = gpd.GeoDataFrame(
+        df,
+        geometry=geometry,
+        crs=crs
+    )
+
+    return gdf
+
+def edges_csv_to_gdf(
+    edges_csv: str = "client/public/edges_table.csv",
+    geom_csv: str = "client/public/edges_geometry.csv",
+    crs="EPSG:4326"
+) -> gpd.GeoDataFrame:
+
+    edges_df = pd.read_csv(edges_csv)
+    geom_df = pd.read_csv(geom_csv)
+
+    geom_df["geometry"] = geom_df["geometry"].apply(wkt.loads)
+
+    gdf = gpd.GeoDataFrame(
+        edges_df.merge(
+            geom_df,
+            left_on=["from_node", "to_node", "key"],
+            right_on=["u", "v", "key"],
+            how="inner"
+        ),
+        geometry="geometry",
+        crs=crs
+    )
+
+    return gdf
+
+def edges_in_path(path_of_nodes : list[int], edges_gdf : gpd.GeoDataFrame):
+    pairs = list(zip(path_of_nodes[:-1], path_of_nodes[1:]))
+    route_edges = edges_gdf[
+        edges_gdf.apply(
+            lambda r: (r["from_node"], r["to_node"]) in pairs,
+            axis=1
+        )
+    ]
+    return route_edges
+
+
+def path_to_linestring(route_edges: gpd.GeoDataFrame):
+    return unary_union(route_edges.geometry)
+
+def path_to_geojson(route_edges: gpd.GeoDataFrame) -> dict:
+    geometry = unary_union(route_edges.geometry)
+
+    return {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": geometry.__geo_interface__,
+                "properties": {}
+            }
+        ]
+    }
+
+if __name__ == "__main__":
+    print(edges_csv_to_gdf())
--- server/app/api/helpers/route_helper.py
+++ server/app/api/helpers/route_helper.py
@@ -1,8 +1,17 @@
 from server.app.domain.routing.algorithms.yen_algorithm import yens
-from server.app.api.helpers.algorithm_helper import get_dict_of_edges
+from server.app.api.helpers.algorithm_helper import get_dict_of_edges, edges_csv_to_gdf, nodes_csv_to_gdf, edges_in_path, path_to_geojson
 import networkx as nx
+import osmnx as ox
+from pathlib import Path
 
 from flask import Blueprint, request, jsonify
+
+
+from client.public.convert import (
+    load_edge_geometries,
+    build_nodes_geojson,
+    build_edges_geojson,
+)
 
 from server.app.domain.scoring.cost_functions import HS_ATTRIBUTES, explain_cost
 from server.app.domain.scoring.weight_utils import DEFAULT_WEIGHTS, validate_weights
@@ -14,21 +23,46 @@ from server.app.api.utils.error_utils import (
 
 route_bp = Blueprint("route", __name__)
 
+DATA_PATH = Path("client/public")
+NODES_CSV = DATA_PATH / "nodes_table.csv"
+EDGES_CSV = DATA_PATH / "edges_table.csv"
+GEOM_CSV = DATA_PATH / "edges_geometry.csv"
+
+edge_geometries = load_edge_geometries(GEOM_CSV)
+nodes_geojson = build_nodes_geojson(NODES_CSV)
+edges_geojson = build_edges_geojson(EDGES_CSV, edge_geometries)
+
 JSON_PATH = "server/app/api/helpers/edge_list.json"
 
+NODES_GDF = nodes_csv_to_gdf()
+EDGES_GDF = edges_csv_to_gdf()
+
+GRAPH = ox.graph_from_gdfs(NODES_GDF, EDGES_GDF)
+
 @route_bp.route("/routing", methods=["GET"])
-def run_route_algorithm(graph : nx.MultiDiGraph, start_node : int, end_node : int, route_algorithm : function = yens):
-    edge_list = get_dict_of_edges(graph)
-    shortestPaths = route_algorithm(edge_list, start_node, end_node)
-    paths = {}
+def run_route_algorithm():
+    start_node = request.args.get("start", type=int)
+    end_node = request.args.get("end", type=int)
+
+    if start_node is None or end_node is None:
+        return jsonify({"error": "Start and end nodes required"}), 400
+
+    edge_list = get_dict_of_edges(GRAPH)
+    if start_node not in GRAPH or end_node not in GRAPH:
+        return jsonify({"error" : "Invalid start or end node"}), 400
+    
+    shortest_paths = yens(edge_list, start_node, end_node)
+    route_geojson = {}
     order = ["shortestPath", "secondPath", "thirdPath"]
-    for path, name in zip(shortestPaths, order):
-        paths[name] = path
-    return jsonify({"paths" : paths})
+    
+    for name, path in zip(order, shortest_paths):
+        route_edges = edges_in_path(path, EDGES_GDF)
+        route_geojson[name] = path_to_geojson(route_edges)
+
+    return jsonify(route_geojson)
     
 
 def edges_to_json(edge_list : list):
     import json
     with open(JSON_PATH, 'w') as f:
         json.dump(edge_list, f, ensure_ascii=False, indent=4)
-    return json.dump(edge_list, f, ensure_ascii=False, indent=4)
