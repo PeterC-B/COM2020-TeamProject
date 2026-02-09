699fc334701a2b2b2762b7c7ba84abf0f165473e
sophiebarrell <sb1522@exeter.ac.uk>
2026-02-09 14:43:36 +0000

    script added so sliders update correctly

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ safety_output.textContent = safety_slider.value;
 safety_slider.addEventListener("input", function () {
     safety_output.textContent = this.value;
 });
+
+const lighting_slider = document.getElementById("lighting_range")
+const lighting_output = document.getElementById("lighting_span")
+
+lighting_output.textContent = lighting_slider.value;
+
+lighting_slider.addEventListener("input", function () {
+    lighting_output.textContent = this.value;
+});
+
+const green_slider = document.getElementById("green_range")
+const green_output = document.getElementById("green_span")
+
+green_output.textContent = green_slider.value;
+
+green_slider.addEventListener("input", function () {
+    green_output.textContent = this.value;
+});
+
+const crossings_slider = document.getElementById("crossings_range")
+const crossings_output = document.getElementById("crossings_span")
+
+crossings_output.textContent = crossings_slider.value;
+
+crossings_slider.addEventListener("input", function () {
+    crossings_output.textContent = this.value;
+});
