cd225a29952ca9a4ea1b5eaecbdc72ebc5ddb1d3
PeterC-B <97701071+PeterC-B@users.noreply.github.com>
2026-02-10 19:10:16 +0000

    Refactored /graph route to use the use_case + repo structure

--- server/app/api/graph/graph_endpoints.py
+++ server/app/api/graph/graph_endpoints.py
@@ -1,57 +1,11 @@
-from pathlib import Path
-
 from flask import Blueprint, jsonify
 
-from server.data.convert import (build_edges_geojson, build_nodes_geojson,
-                                 load_edge_geometries)
 
-DATA_PATH = Path("server/data/processed")
-NODES_CSV = DATA_PATH / "nodes_table.csv"
-EDGES_CSV = DATA_PATH / "edges_table.csv"
-GEOM_CSV = DATA_PATH / "edges_geometry.csv"
-
-
-JSON_PATH = "server/app/api/helpers/edge_list.json"
-
-# NODES_GDF = help_algos.nodes_csv_to_gdf()
-# EDGES_GDF = help_algos.edges_csv_to_gdf()
-
-
-
-def create_graph_route_blueprint():
-
+def create_graph_route_blueprint(get_graph_data_uc):
     bp = Blueprint("graph", __name__, url_prefix="/graph")
 
-    # Main endpoint to get graph data (nodes + edges)
     @bp.route("", methods=["GET"])
     def get_graph_data():
+        return jsonify(get_graph_data_uc.execute())
 
-        edge_geometries = load_edge_geometries(GEOM_CSV)
-        nodes_geojson = build_nodes_geojson(NODES_CSV)
-        edges_geojson = build_edges_geojson(EDGES_CSV, edge_geometries)
-
-        all_features = {
-            "nodes" : nodes_geojson,
-            "edges" : edges_geojson
-        }
-
-        return jsonify({"features" : all_features})
-    
     return bp
-    
-
-        
-        
-        
-    
-
-        
-        
-        
-    
-
-        
-        
-
-        
-        
