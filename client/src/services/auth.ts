94cdb36c3eedebcc502369c7f2c89af2fe979d40
PeterC-B <97701071+PeterC-B@users.noreply.github.com>
2026-02-10 15:56:36 +0000

    Added in basic login system on the backend and frontend. Need to decide if it should be username & password or if email is needed as well

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -2,13 +2,13 @@ import { post } from '@/services/api'
 
 type LoginResponse = {
     access_token?: string
-    token?: string
-    jwt?: string
 }
 
 export async function login(username: string, password: string) {
-    const data = await post<LoginResponse>('/login', { username, password }).then(({ data }) => data)
-    const token = data.access_token ?? data.token ?? data.jwt
+    const payload = await post<{ data?: LoginResponse }>('/user/login', { username, password }).then(
+        ({ data }) => data,
+    )
+    const token = payload.data?.access_token
 
     if (!token) {
         throw new Error('Login response did not include a token')
