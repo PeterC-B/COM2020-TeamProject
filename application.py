74173e3d69e10e65fe8167d1cc4d757324e1fa7c
Breheny2 <eb1151@exeter.ac.uk>
2026-02-12 18:13:13 +0000

    all of my work

---
+++ application.py
@@ -0,0 +1,10 @@
+# application.py (located in your root folder)
+
+from server.app import app as application
+
+# If your Flask variable inside app.py is named 'app', 
+# this line imports it and renames it to 'application' 
+# so AWS can find it automatically.
+
+if __name__ == "__main__":
+    application.run()
