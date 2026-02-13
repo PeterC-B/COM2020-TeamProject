c1e7bd18595ace8fd4f39059b091fd6ff17a6100
PeterC-B <97701071+PeterC-B@users.noreply.github.com>
2026-02-13 15:18:49 +0000

    Implemented a standard .gitignore template

--- server/app/config.py
+++ server/app/config.py
@@ -16,15 +16,45 @@ class Config:
 
     def get_config(self):
 
-        DB_NAME = config_dict.get('DB_NAME')
-        DB_USERNAME = config_dict.get('DB_USERNAME')
-        DB_PASSWORD = config_dict.get('DB_PASSWORD')
-        DB_HOST = config_dict.get('DB_HOST')
-        DB_PORT = config_dict.get('DB_PORT')
+        env = config_dict.get('ENV')
+        if env == 'production':
+            self.APP_ENV = 'PROD'
+            print("App started in Production environment")
+        elif env == 'development':
+            self.APP_ENV = 'DEV'
+            print("App started in Development environment")
+        elif env == 'testing':
+            self.APP_ENV = 'TEST'
+            print("App started in Testing environment")
+        else:
+            raise ValueError("Invalid environment")
 
         self.SECRET_KEY = config_dict.get('SECRET_KEY')
 
-        self.DB_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+        DB_NAME = config_dict.get(f'{self.APP_ENV}_DB_NAME')
+        DB_USERNAME = config_dict.get(f'{self.APP_ENV}_DB_USERNAME')
+        DB_PASSWORD = config_dict.get(f'{self.APP_ENV}_DB_PASSWORD')
+        DB_HOST = config_dict.get(f'{self.APP_ENV}_DB_HOST')
+        DB_PORT = config_dict.get(f'{self.APP_ENV}_DB_PORT')
 
-        if any(attr is None for attr in [self.SECRET_KEY, self.DB_URI, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT]):
-            raise ValueError("One or more required environment variables are missing.")
+        if env == 'production':
+            self.DB_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require'
+            # print("SSL Enabled")
+        else:
+            self.DB_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+            # print("SSL Disabled")
+
+        CORS_ADDRESSES = config_dict.get(f'{self.APP_ENV}_CORS_ADDRESSES')
+        self.CORS_ADDRESSES = CORS_ADDRESSES.split(',')
+
+        # self.print_config()
+
+        # Check all successfull
+        if any(attr is None for attr in [self.APP_ENV, self.SECRET_KEY, self.DB_URI, self.CORS_ADDRESSES]):
+            raise ValueError("Error setting up app config")
+        
+
+    def print_config(self):
+        print("Current Configuration:")
+        print(f"SECRET_KEY: {'***' if self.SECRET_KEY else None}")
+        print(f"DB_URI: {self.DB_URI}")
