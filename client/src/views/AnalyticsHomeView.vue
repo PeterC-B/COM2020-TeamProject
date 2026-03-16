8b96fb3d0c806a472635807e0e1dea30a73908e1
NotEthanFranklin74 <chamax123@outlook.com>
2026-03-16 15:54:23 +0000

    Added analytics home page CSS

--- client/src/views/AnalyticsHomeView.vue
+++ client/src/views/AnalyticsHomeView.vue
@@ -5,7 +5,7 @@
         <div class="mt-10 flex items-center gap-x-2 border-b border-slate-200">
             <router-link
                 to="/analytics/route-queries"
-                class="px-6 py-3 text-sm font-bld transition-all hover:text-indigo-600 border-b-2 border-transparent hover:border-indigo-600 text-slate-500"
+                class="px-6 py-3 text-sm font-bold transition-all hover:text-indigo-600 border-b-2 border-transparent hover:border-indigo-600 text-slate-500"
                 active-class="text-indigo-600 border-indigo-600 bg-indigo-50/30"
             >
                 Route Queries
